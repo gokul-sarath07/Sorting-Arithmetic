580724c8a153427636150b690ea0f9538369b78f
gokul-sarath07 <gokul510.s@gmail.com>
2020-08-04 01:08:31 +0530

    Dictionary to Array

--- program.sh
+++ program.sh
@@ -8,3 +8,10 @@ dict[UC_4]=$(( $c + $a / $b ))
 dict[UC_5]=$(( $a % $b + $c ))
 echo "All the values: " ${dict[@]}
 echo "All the keys: " ${!dict[@]}
+ct=0
+for value in ${dict[@]}
+do
+    array[$ct]=$value
+    ct=$(( $ct + 1 ))
+done
+echo "Array values: " ${array[@]}
