2a708e758dc02fe26a87fb404a88fd84711d1edc
gokul-sarath07 <gokul510.s@gmail.com>
2020-08-04 00:58:04 +0530

    Stored in Dictionary

--- program.sh
+++ program.sh
@@ -1,7 +1,10 @@
 #!/bin/bash -x
 echo "Sorting Arithmetic Computation"
 read -p "Enter three inputs: " a b c
-echo $(( $a + $b * $c ))
-echo $(( $a * $b + $c ))
-echo $(( $c + $a / $b ))
-echo $(( $a % $b + $c ))
+declare -A dict
+dict[UC_2]=$(( $a + $b * $c ))
+dict[UC_3]=$(( $a * $b + $c ))
+dict[UC_4]=$(( $c + $a / $b ))
+dict[UC_5]=$(( $a % $b + $c ))
+echo "All the values: " ${dict[@]}
+echo "All the keys: " ${!dict[@]}
