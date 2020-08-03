0e85dece542bbb0ea7015f1d634a592604e389c7
gokul-sarath07 <gokul510.s@gmail.com>
2020-08-04 00:45:28 +0530

    Computed a % b + c and Added Heading

--- program.sh
+++ program.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -x
+echo "Sorting Arithmetic Computation"
 read -p "Enter three inputs: " a b c
 echo $(( $a + $b * $c ))
 echo $(( $a * $b + $c ))
 echo $(( $c + $a / $b ))
+echo $(( $a % $b + $c ))
