04566bd722ee0922bf005304e5ebe4b1f77794b3
panic-coder <androidshubham@gmail.com>
2020-08-04 19:52:13 +0530

    logic change

--- program.sh
+++ program.sh
@@ -6,7 +6,7 @@ declare -A dict
 dict[UC_2]=$(( $a + $b * $c ))
 dict[UC_3]=$(( $a * $b + $c ))
 dict[UC_4]=$(( $c + $a / $b ))
-dict[UC_5]=$(( $a % $b + $c ))
+dict[UC_5]=$(( $a * $b - $c ))
 echo "All the values of dictionary: " ${dict[@]}
 echo "All the keys dictionary: " ${!dict[@]}
 ct=0
