73bcbe09e5f2e786c75acccde71db63ea750442d
gokul-sarath07 <gokul510.s@gmail.com>
2020-08-04 19:22:36 +0530

    Changed to Ascending

--- program.sh
+++ program.sh
@@ -15,27 +15,27 @@ do
 	ct=$(( $ct + 1 ))
 done
 echo "Array values: " ${array[@]}
-des=()
+asc=()
 while [ ${#array[@]} -gt 1 ]
 do
-	last=${array[0]}
-	for i in ${array[@]}
-	do
-		if [[ $i -gt $last ]]
-		then
-			last="$i"
-		fi
-	done
-	des+=($last)
-	for i in ${!array[@]}
-	do
-		if [[ ${array[i]} -eq $last ]]
-		then
-			index=$i
-			break
-		fi
-	done
-	unset array[$index]
+    first=${array[0]}
+    for i in ${array[@]}
+    do
+        if [[ $i -lt $first ]]
+        then
+            first="$i"
+        fi
+    done
+    asc+=($first)
+    for i in ${!array[@]}
+    do
+        if [[ ${array[i]} -eq $first ]]
+        then
+            index=$i
+            break
+        fi
+    done
+    unset array[$index]
 done
-des=( "${des[@]}" "${array[@]}" )
-echo "Descending Order: " ${des[@]}
+asc=( "${asc[@]}" "${array[@]}" )
+echo "Ascending Order: " ${asc[@]}
